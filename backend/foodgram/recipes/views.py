0513867220c4689a8af7a0e8a51d618565cded84
Sergey Samoylenko <s.r.samoylenko@gmail.com>
2021-10-17 13:56:11 +0300

    shopping cart endpoints added.

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
@@ -37,10 +36,14 @@ class RecipeViewSet(ModelViewSet):
     def get_permissions(self):
         if self.action == 'favorite':
             self.permission_classes = [permissions.IsAuthenticated]
+        elif self.action == 'shopping_cart':
+            self.permission_classes = [permissions.IsAuthenticated]
         return super().get_permissions()
 
     def get_serializer_class(self):
         if self.action == 'favorite':
+            return RecipeShortSerializer
+        elif self.action == 'shopping_cart':
             return RecipeShortSerializer
         return super().get_serializer_class()
 
@@ -93,5 +96,54 @@ class RecipeViewSet(ModelViewSet):
         if recipe not in user.favorites.recipes.all():
             if raise_exception:
                 raise ValidationError(_('Not a favorite recipe.'))
+            return False
+        return True
+
+    @action(
+        detail=True,
+        methods=('get', 'delete'),
+    )
+    def shopping_cart(self, request, pk=None):
+        operation = {
+            'GET': self.add_to_shopping_cart,
+            'DELETE': self.remove_from_shopping_cart,
+        }
+        return operation[request.method](request, pk)
+
+    def add_to_shopping_cart(self, request, pk=None):
+        user = request.user
+        recipe = get_object_or_404(Recipe, pk=pk)
+
+        if self.can_add_to_shopping_cart(user, recipe, raise_exception=True):
+            user.shopping_cart.recipes.add(recipe)
+
+        context = {'request': request}
+        serializer = self.get_serializer(
+            recipe,
+            context=context,
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @staticmethod
+    def can_add_to_shopping_cart(user, recipe, raise_exception=True):
+        if recipe in user.shopping_cart.recipes.all():
+            if raise_exception:
+                raise ValidationError(_('Cannot add to shopping cart twice.'))
+            return False
+        return True
+
+    def remove_from_shopping_cart(self, request, pk=None):
+        user = request.user
+        recipe = get_object_or_404(Recipe, id=id)
+
+        if self.can_remove_from_shopping_cart(user, recipe, raise_exception=True):
+            user.shopping_cart.recipes.remove(recipe)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @staticmethod
+    def can_remove_from_shopping_cart(user, recipe, raise_exception=True):
+        if recipe not in user.shopping_cart.recipes.all():
+            if raise_exception:
+                raise ValidationError(_('Not in shopping cart.'))
             return False
         return True
